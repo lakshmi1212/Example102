5e44b470f42e1a4fbc090535fb4b1852989fb5cc
lakshmi1212 <36476419+lakshmi1212@users.noreply.github.com>
2026-02-16 19:24:34 +0530

    Add or update file: test_subtract.py

--- tests/test_subtract.py
+++ tests/test_subtract.py
@@ -2,19 +2,21 @@ import pytest
 from src.math_operations import subtract
 
 def test_subtract_positive_numbers():
-    assert subtract(5, 3) == 2
+    assert subtract(5, 2) == 3
+    assert subtract(20, 10) == 10
 
 def test_subtract_negative_numbers():
-    assert subtract(-5, -3) == -2
+    assert subtract(-1, -2) == 1
+    assert subtract(-10, 5) == -15
 
 def test_subtract_zero():
     assert subtract(0, 0) == 0
-
-def test_subtract_positive_and_negative():
-    assert subtract(5, -3) == 8
+    assert subtract(0, 5) == -5
+    assert subtract(5, 0) == 5
 
 def test_subtract_floats():
-    assert subtract(5.5, 2.2) == pytest.approx(3.3)
+    assert subtract(2.5, 1.5) == 1.0
+    assert subtract(-1.1, 1.1) == -2.2
 
 def test_subtract_large_numbers():
-    assert subtract(2_000_000, 1_000_000) == 1_000_000
+    assert subtract(2_000_000_000, 1_000_000_000) == 1_000_000_000
